ec46b0f4624f25098be02be5dbe3d73a3d0d5f3c
dkt64 <b.apanasewicz@gmail.com>
2019-08-20 14:56:58 +0200

    port 80

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
     devServer: {
-      port: 8090,
+      port: 80,
     },
     configureWebpack: {
       devtool: 'source-map'
